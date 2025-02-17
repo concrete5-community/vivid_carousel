4b3e96798aaaac2bb1bfaacbbf21d3c1cf18282e
Michele Locati <michele@locati.it>
2025-02-17 16:24:55 +0100

    package controller: add phpdoc, install via CIF, require core >= 8.5.2

--- controller.php
+++ controller.php
@@ -1,35 +1,69 @@
-<?php      
+<?php
 
 namespace Concrete\Package\VividCarousel;
-use Package;
-use BlockType;
 
-defined('C5_EXECUTE') or die(_("Access Denied."));
+use Concrete\Core\Package\Package;
+
+defined('C5_EXECUTE') or die('Access Denied.');
 
 class Controller extends Package
 {
+    /**
+     * @var string
+     */
+    protected $pkgHandle = 'vivid_carousel';
 
-	protected $pkgHandle = 'vivid_carousel';
-	protected $appVersionRequired = '5.7.1';
-	protected $pkgVersion = '1.0.1';
-	
-	
-	
-	public function getPackageDescription()
-	{
-		return t("Add a Carousel to your Site");
-	}
+    /**
+     * @var string
+     */
+    protected $pkgVersion = '1.0.1';
 
-	public function getPackageName()
-	{
-		return t("Vivid Carousel");
-	}
-	
-	public function install()
-	{
-		$pkg = parent::install();
-        BlockType::installBlockTypeFromPackage('vivid_carousel', $pkg); 
-        
-	}
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Package\Package::$appVersionRequired
+     */
+    protected $appVersionRequired = '8.5.2';
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Package\Package::getPackageName()
+     */
+    public function getPackageName()
+    {
+        return t('Vivid Carousel');
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Package\Package::getPackageDescription()
+     */
+    public function getPackageDescription()
+    {
+        return t('Add a carousel to your site');
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Package\Package::install()
+     */
+    public function install()
+    {
+        parent::install();
+        $this->installContentFile('config/install.xml');
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Package\Package::upgrade()
+     */
+    public function upgrade()
+    {
+        parent::upgrade();
+        $this->installContentFile('config/install.xml');
+    }
 }
-?>
